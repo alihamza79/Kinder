5116263508e88ab3d7934a783a3047b01e328c7b
Ali Hamza <hamzachaudhay79@gmail.com>
2024-07-05 17:28:50 +0500

    Page should get reload if we land on Login

--- src/Pages/login/index.jsx
+++ src/Pages/login/index.jsx
@@ -1,20 +1,9 @@
-import React  from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-// import FeatherIcon from "feather-icons-react";
 import { login02, loginicon01, loginicon02, loginicon03, loginlogo } from "../../Components/imagepath";
-import "owl.carousel/dist/assets/owl.carousel.css";
-import "owl.carousel/dist/assets/owl.theme.default.css";
-import { useState } from "react";
-
 import { Eye, EyeOff } from "feather-icons-react/build/IconComponents";
 
-// import ReactPasswordToggleIcon from 'react-password-toggle-icon';
-
-
-
 const Login = () => {
-
-
   const [passwordVisible, setPasswordVisible] = useState(false);
   const [password, setPassword] = useState('');
 
@@ -22,17 +11,15 @@ const Login = () => {
     setPasswordVisible(!passwordVisible);
   };
 
+  useEffect(() => {
+    if (!sessionStorage.getItem('reloaded')) {
+      sessionStorage.setItem('reloaded', 'true');
+      window.location.reload();
+    }
+  }, []);
 
-  // let inputRef = useRef();
-  // const showIcon = () => <i className="feather feather-eye" aria-hidden="true">
-  //   <FeatherIcon icon="eye" />
-  // </i>;
-  // const hideIcon = () => <i className="feather feather-eye-slash" aria-hidden="true">
-  //   <FeatherIcon icon="eye-off" />
-  // </i>
   return (
     <>
-
       {/* Main Wrapper */}
       <div className="main-wrapper login-body">
         <div className="container-fluid px-0">
@@ -41,11 +28,7 @@ const Login = () => {
             <div className="col-lg-6 login-wrap">
               <div className="login-sec">
                 <div className="log-img">
-                  <img
-                    className="img-fluid"
-                    src={login02}
-                    alt="#"
-                  />
+                  <img className="img-fluid" src={login02} alt="#" />
                 </div>
               </div>
             </div>
@@ -63,7 +46,7 @@ const Login = () => {
                       </div>
                       <h2>Login</h2>
                       {/* Form */}
-                      <form >
+                      <form>
                         <div className="form-group">
                           <label>
                             Email <span className="login-danger">*</span>
@@ -75,17 +58,14 @@ const Login = () => {
                             Password <span className="login-danger">*</span>
                           </label>
                           <input
-                          type={passwordVisible ? 'password' : ''}
-                          className="form-control pass-input"
-                          value={password}
-                          onChange={(e) => setPassword(e.target.value)}
-                        />
-                        <span
-                          className="toggle-password"
-                          onClick={togglePasswordVisibility}
-                        >
-                          {passwordVisible ? <EyeOff className="react-feather-custom" /> : <Eye className="react-feather-custom" />}
-                        </span>
+                            type={passwordVisible ? 'text' : 'password'}
+                            className="form-control pass-input"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                          />
+                          <span className="toggle-password" onClick={togglePasswordVisibility}>
+                            {passwordVisible ? <EyeOff className="react-feather-custom" /> : <Eye className="react-feather-custom" />}
+                          </span>
                         </div>
                         <div className="forgotpass">
                           <div className="remember-me">
@@ -99,10 +79,7 @@ const Login = () => {
                           <Link to="/forgotpassword">Forgot Password?</Link>
                         </div>
                         <div className="form-group login-btn">
-                          <Link to="/admin-dashboard"
-                            className="btn btn-primary btn-block"
-
-                          >
+                          <Link to="/admin-dashboard" className="btn btn-primary btn-block">
                             Login
                           </Link>
                         </div>
@@ -115,13 +92,13 @@ const Login = () => {
                         {/* Social Login */}
                         <div className="social-login">
                           <Link to="#">
-                            <img src={loginicon01} alt="#"/>
+                            <img src={loginicon01} alt="#" />
                           </Link>
                           <Link to="#">
-                            <img src={loginicon02}alt="#" />
+                            <img src={loginicon02} alt="#" />
                           </Link>
                           <Link to="#">
-                            <img src={loginicon03}  alt="#"/>
+                            <img src={loginicon03} alt="#" />
                           </Link>
                         </div>
                         {/* /Social Login */}
