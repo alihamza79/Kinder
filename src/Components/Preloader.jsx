fb060ca3eb8ddb29e3c802e727b7f23a8a5015f9
“Husnain-Ashfaq39” <127616463+Husnain-Ashfaq39@users.noreply.github.com>
2024-07-14 10:45:11 +0500

    setup loader at startup and login

---
+++ src/Components/Preloader.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import '../Assets/css/Preloader.scss'
+function Preloader() {
+  return (
+   <>
+    <div id="container50">
+  <div class="divider" aria-hidden="true"></div>
+  <p class="loading-text" aria-label="Loading">
+    <span class="letter" aria-hidden="true">L</span>
+    <span class="letter" aria-hidden="true">o</span>
+    <span class="letter" aria-hidden="true">a</span>
+    <span class="letter" aria-hidden="true">d</span>
+    <span class="letter" aria-hidden="true">i</span>
+    <span class="letter" aria-hidden="true">n</span>
+    <span class="letter" aria-hidden="true">g</span>
+  </p>
+</div>
+
+
+
+</>
+  )
+}
+
+export default Preloader
