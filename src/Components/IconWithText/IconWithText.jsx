b6c0fcc60a731e000af7fc136559898c8eddb06e
“Husnain-Ashfaq39” <127616463+Husnain-Ashfaq39@users.noreply.github.com>
2024-07-15 13:24:57 +0500

    border reversed

--- src/Components/IconWithText/IconWithText.jsx
+++ src/Components/IconWithText/IconWithText.jsx
@@ -5,7 +5,6 @@ import { PropTypes } from "prop-types";
 import { Link } from 'react-router-dom';
 import Buttons from '../Button/Buttons';
 import { IconWithTextData_01 } from './IconWithTextData';
-import './border.scss'
 
 const IconWithText = (props) => {
   return (
@@ -16,33 +15,29 @@ const IconWithText = (props) => {
           className={`col ${props.theme ? ` ${props.theme}` : ""}${props.className ? ` ${props.className}` : ""} flex`}
           {...{ ...props.animation, transition: { delay: i * props.animationDelay, ease: props.animationTransition, duration: props.animationDuration } }}
         >
-
-          <div href="#" className='box'>
-            <div className="rounded-md w-full flex flex-row items-center">
-              {item.img ? (
-                <img height={42} width={51} className="inline-block items-center justify-center mr-[30px]" src={item.img} alt="featurebox" />
-              ) : item.icon ? (
-                props.theme === "icon-with-text-05" ? (
-                  <Link aria-label="link for icon" to="#">
-                    <i className={`${item.icon} mr-[30px]`}></i>
-                  </Link>
-                ) : (
+          <div className="rounded-md w-full flex flex-row items-center">
+            {item.img ? (
+              <img height={42} width={51} className="inline-block items-center justify-center mr-[30px]" src={item.img} alt="featurebox" />
+            ) : item.icon ? (
+              props.theme === "icon-with-text-05" ? (
+                <Link aria-label="link for icon" to="#">
                   <i className={`${item.icon} mr-[30px]`}></i>
-                )
-              ) : item.textIcon ? (
-                <span className="text-basecolor inline-block icon-text mr-[30px]">{item.textIcon}</span>
+                </Link>
               ) : (
-                <span className="text-basecolor inline-block icon-text mr-[30px]">{`${i <= 9 ? "0" : ""}${i + 1}`}</span>
-              )}
+                <i className={`${item.icon} mr-[30px]`}></i>
+              )
+            ) : item.textIcon ? (
+              <span className="text-basecolor inline-block icon-text mr-[30px]">{item.textIcon}</span>
+            ) : (
+              <span className="text-basecolor inline-block icon-text mr-[30px]">{`${i <= 9 ? "0" : ""}${i + 1}`}</span>
+            )}
 
-              <div className="feature-box-content flex-grow">
-                {item.title && <span className="font-medium title font-serif">{item.title}</span>}
-                {item.content && <p dangerouslySetInnerHTML={{ __html: item.content }} />}
-                {(item.linkTitle || item.link) && <Buttons ariaLabel="iconwithtext" className="font-medium font-serif uppercase btn-link after:h-[1px] md:text-md md:mb-[15px] after:bg-basecolor hover:text-basecolor mt-auto" to={item.link} title={item.linkTitle} />}
-              </div>
+            <div className="feature-box-content flex-grow">
+              {item.title && <span className="font-medium title font-serif">{item.title}</span>}
+              {item.content && <p dangerouslySetInnerHTML={{ __html: item.content }} />}
+              {(item.linkTitle || item.link) && <Buttons ariaLabel="iconwithtext" className="font-medium font-serif uppercase btn-link after:h-[1px] md:text-md md:mb-[15px] after:bg-basecolor hover:text-basecolor mt-auto" to={item.link} title={item.linkTitle} />}
             </div>
           </div>
-
         </m.div>
       ))}
     </Row>
@@ -77,3 +72,4 @@ IconWithText.propTypes = {
 };
 
 export default memo(IconWithText);
+
